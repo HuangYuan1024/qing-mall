fbe5bd08adf35ae0d32a67b8856915efed5f32b5
HuangYuan1024 <3205518128@qq.com>
2025-11-11 20:28:03 +0800

    完善商品业务
    Signed-off-by: HuangYuan1024 <3205518128@qq.com>

---
+++ code/business/goods-service/goods-infrastructure/src/main/java/com/huangyuan/goodsinfrastructure/persistence/mapper/SkuMapper.java
@@ -0,0 +1,7 @@
+package com.huangyuan.goodsinfrastructure.persistence.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface SkuMapper {
+}
---
+++ code/business/goods-service/goods-infrastructure/src/main/java/com/huangyuan/goodsinfrastructure/persistence/mapper/SpuMapper.java
@@ -0,0 +1,8 @@
+package com.huangyuan.goodsinfrastructure.persistence.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface SpuMapper {
+
+}
