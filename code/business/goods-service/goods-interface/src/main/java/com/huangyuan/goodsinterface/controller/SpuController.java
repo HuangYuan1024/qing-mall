425e2f834d506876ff8dc7793866deed0402b4a0
HuangYuan1024 <3205518128@qq.com>
2025-11-12 20:22:52 +0800

    完善商品业务
    Signed-off-by: HuangYuan1024 <3205518128@qq.com>

---
+++ code/business/goods-service/goods-interface/src/main/java/com/huangyuan/goodsinterface/controller/SpuController.java
@@ -0,0 +1,27 @@
+package com.huangyuan.goodsinterface.controller;
+
+import com.huangyuan.goodsapplication.command.CreateSpuCommand;
+import com.huangyuan.goodsapplication.service.command.SpuCommandAppService;
+import com.huangyuan.qingcommon.result.RespResult;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/spu")
+public class SpuController {
+
+    private final SpuCommandAppService commandService;
+
+    /**
+     * 保存商品
+     */
+    @RequestMapping("/save")
+    public RespResult<Void> save(@Valid @RequestBody CreateSpuCommand createSpuCommand) {
+        commandService.createSpu(createSpuCommand);
+        return RespResult.ok();
+    }
+}
