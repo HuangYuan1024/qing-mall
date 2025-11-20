15cab3a72773558738d9c639a5c3ff351eb4cdec
HuangYuan1024 <3205518128@qq.com>
2025-11-20 15:53:45 +0800

    引入RabbitMQ实现Redis缓存异步刷新
    Signed-off-by: HuangYuan1024 <3205518128@qq.com>

--- code/business/goods-service/goods-interface/src/main/java/com/huangyuan/goodsinterface/controller/SpuController.java
+++ code/business/goods-service/goods-interface/src/main/java/com/huangyuan/goodsinterface/controller/SpuController.java
@@ -7,6 +7,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,6 +23,13 @@ public class SpuController {
     @RequestMapping("/save")
     public RespResult<Void> save(@Valid @RequestBody CreateSpuCommand createSpuCommand) {
         commandService.createSpu(createSpuCommand);
+        return RespResult.ok();
+    }
+
+    @RequestMapping("/updateStatus")
+    public RespResult<Void> updateStatus(@RequestParam("spuId") String spuId,
+                                         @RequestParam("statusCode") Integer statusCode) {
+        commandService.updateGoodsStatus(spuId, statusCode);
         return RespResult.ok();
     }
 }
